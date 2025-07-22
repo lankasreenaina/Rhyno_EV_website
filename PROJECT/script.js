7b9127ebe6fa4ba5a8c79b8934822e5314afe6e0
SREENAINA <sreenaina0711@gmail.com>
2025-07-22 22:45:22 +0530

    Initial commit: Add project files for Rhyno_EV_website

---
+++ PROJECT/script.js
@@ -0,0 +1,44 @@
+// let arr = ["img/bike1.png", "img/bike2.png", "img/bike3.png"];
+// let i = 0;
+// let count = 0;
+
+// function forward() {
+//     count=count+1;
+//     i=count/3;
+//     const x = document.querySelector("#body");
+//     x.style.backgroundImage = 'url(' + arr[i] + ')';
+// }
+
+// function backward() {
+//     count=count-1;
+//     i=count/3;
+//     const x = document.querySelector("#body");
+//     x.style.backgroundImage = 'url(' + arr[i] + ')';
+// }
+// const x = document.querySelector("#body");
+
+function showSlider(){
+    const mobilescreen1 = document.querySelector('#products')
+    mobilescreen1.style.display = 'flex'
+}
+function hideSlider(){
+    const mobilescreen1 = document.querySelector('#products')
+    mobilescreen1.style.display = 'none'
+}
+function showSlider1(){
+    const mobilescreen1 = document.querySelector('.options')
+    mobilescreen1.style.display = 'flex'
+}
+function hideSlider1(){
+    const mobilescreen1 = document.querySelector('.options')
+    mobilescreen1.style.display = 'none'
+}
+
+function showSlider2(){
+    const mobilescreen1 = document.querySelector('#products1')
+    mobilescreen1.style.display = 'flex'
+}
+function hideSlider2(){
+    const mobilescreen1 = document.querySelector('#products1')
+    mobilescreen1.style.display = 'none'
+}
